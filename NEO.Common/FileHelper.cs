9572ea76bbd39a4bf63abd709777b6e30dd7ca67
neoshero <neoshero@163.com>
2018-04-26 22:23:46 +0800

    Init Repository

---
+++ NEO.Common/FileHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NEO.Common
+{
+    public class FileHelper
+    {
+        /// <summary>
+        /// 写入文件
+        /// </summary>
+        /// <param name="buffer">字节流</param>
+        /// <param name="filename">文件名称</param>
+        public void Write(byte[] buffer, string filename)
+        {
+            var folder = Path.GetDirectoryName(filename);
+            
+            if (string.IsNullOrEmpty(folder))
+            {
+                folder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "File");
+            }
+
+            if(!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+
+            var ms = new MemoryStream(buffer);
+            var writer  = new StreamWriter(ms);
+            writer.Flush();
+            ms.Flush();
+
+        }
+    }
+}
---
+++ NEO.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NEO.Common;
+
+namespace NEO.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogHelper.Log("11111111111111");
+            LogHelper.Log("2222222222222222");
+        }
+    }
+}
