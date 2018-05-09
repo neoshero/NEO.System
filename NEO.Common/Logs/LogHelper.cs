34ba437e23ead6ab89fc9067d2d20778e72956a9
nier <nierpeng@dynasys.com.hk>
2018-05-09 10:25:23 +0800

    log directory

---
+++ NEO.Common/Logs/LogHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace NEO.Common.Log
+{
+    public class LogHelper
+    {
+        public static void Log(string content)
+        {
+            var filename = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"Log" ,DateTime.Today.ToString("yyyy-MM-dd") + ".txt");
+            var folder = Path.GetDirectoryName(filename);
+
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+
+            using (var filestream = new FileStream(filename,FileMode.Append,FileAccess.Write))
+            {
+                var buffer = Encoding.Default.GetBytes(content + "\n");
+                filestream.Write(buffer,0,buffer.Length);
+                filestream.Flush();
+            }
+        }
+    }
+}
