18a283d4d1a88d51ee2b6bcc0940142f4de41054
nier <nierpeng@dynasys.com.hk>
2018-05-25 18:14:10 +0800

    cache file

--- NEO.Common/Cache/MemoryHelper.cs
+++ NEO.Common/Cache/MemoryHelper.cs
@@ -4,38 +4,38 @@ namespace NEO.Common.Cache
 {
     public class MemoryHelper
     {
-        private static readonly MemoryCache cache = MemoryCache.Default;
+        private static readonly MemoryCache Cache = MemoryCache.Default;
 
         public static void Add<TValue>(string key,TValue value)
         {
-            if (cache.Contains(key))
+            if (Cache.Contains(key))
             {
-                cache[key] = value;
+                Cache[key] = value;
             }
             else
             {
-                cache.Add(key, value,null);
+                Cache.Add(key, value,null);
             }
         }
 
         public static bool Contains(string key)
         {
-            return cache.Contains(key);
+            return Cache.Contains(key);
         }
 
         public static TValue Get<TValue>(string key)
         {
-            return (TValue) cache[key];
+            return (TValue) Cache[key];
         }
 
         public static void Remove(string key)
         {
-            cache.Remove(key);
+            Cache.Remove(key);
         }
 
         public void Clear()
         {
-            foreach (var item in cache)
+            foreach (var item in Cache)
             {
                 Remove(item.Key);
             }
